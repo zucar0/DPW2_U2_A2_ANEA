9f5872026639d4078f1fa1745e0944ec5d3a17a5
Antonio <antonio.enralv@gmail.com>
2021-03-01 08:00:09 -0600

    Validar contraseña

--- guardar.php
+++ guardar.php
@@ -11,6 +11,36 @@
    $contrasena=$_POST['contrasena'];
    $confirma=$_POST['confirma'];   
    $tipousuario='E';
+   $error_encontrado="";
+
+   function validar_clave($contrasena,&$error_clave){
+    if(strlen($contrasena) < 8){
+       $error_clave = "La contraseña debe tener al menos 8 caracteres";
+       return false;
+    }
+    if(strlen($contrasena) > 15){
+       $error_clave = "La contraseña no puede tener más de 15 caracteres";
+       return false;
+    }
+    if (!preg_match('`[a-z]`',$contrasena)){
+       $error_clave = "La contraseña debe tener al menos una letra minúscula";
+       return false;
+    }
+    if (!preg_match('`[A-Z]`',$contrasena)){
+       $error_clave = "La contraseña debe tener al menos una letra mayúscula";
+       return false;
+    }
+    if (!preg_match('`[0-9]`',$contrasena)){
+       $error_clave = "La contraseña debe tener al menos un caracter numérico";
+       return false;
+    }
+    if (preg_match('/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,15}$/', $contrasena)){
+        return true;
+    }
+    $error_clave = "";
+    return true;
+ }
+
    if(isset($_POST['enviar'])){
         if(empty($matricula)){
             echo"<p class='error'> *** Matrícula es un campo obligatorio y no puede estar vacío *** </p>";
@@ -63,33 +93,38 @@
     }   
 
    if($_POST["contrasena"] === $_POST["confirma"] ){
-       if(!empty($_POST['$nombre']) || !empty($_POST['$matricula'])  || !empty($_POST['$apaterno']) ||  !empty($_POST['$amaterno']) ||  !empty($_POST['$tipousuario']) ||
-          !empty($_POST['$sexo']) || !empty($_POST['$edad']) ||  !empty($_POST['$telefono']) ||  !empty($_POST['$email']) ||  !empty($_POST['$contrasena']) ){
-            try{
-                $stmt = $conn->prepare("INSERT INTO usuarios VALUES(:matricula, :nombre, :apaterno, :amaterno, :tipousuario, :sexo, :edad, :telefono, :email, :contrasena)");
-                $stmt->bindValue(':matricula', $matricula);
-                $stmt->bindValue(':nombre', $nombre);
-                $stmt->bindValue(':apaterno', $apaterno);
-                $stmt->bindValue(':amaterno', $amaterno);
-                $stmt->bindValue(':tipousuario', $tipousuario);
-                $stmt->bindValue(':sexo', $sexo);
-                $stmt->bindValue(':edad', $edad);
-                $stmt->bindValue(':telefono', $telefono);
-                $stmt->bindValue(':email', $email);
-                $stmt->bindValue(':contrasena', $contrasena);
-            
-                $stmt->execute();
-                echo "El registro del alumno $nombre $apaterno $amaterno se realizó con éxito.";
-                echo "<br><br><a href='index.html'>*** Regresar al inicio ***</a>";
-                $conn = null;
-            }catch(PDOException $e){
-                echo $sql . "<br>" . $e->getMessage();
-                echo "*** Ocurrió un error; valida los datos que estás ingresando ***";
+       if(validar_clave($_POST["contrasena"], $error_encontrado)){
+            if(!empty($_POST['$nombre']) || !empty($_POST['$matricula'])  || !empty($_POST['$apaterno']) ||  !empty($_POST['$amaterno']) ||  !empty($_POST['$tipousuario']) ||
+            !empty($_POST['$sexo']) || !empty($_POST['$edad']) ||  !empty($_POST['$telefono']) ||  !empty($_POST['$email']) ||  !empty($_POST['$contrasena']) ){
+                try{
+                    $stmt = $conn->prepare("INSERT INTO usuarios VALUES(:matricula, :nombre, :apaterno, :amaterno, :tipousuario, :sexo, :edad, :telefono, :email, :contrasena)");
+                    $stmt->bindValue(':matricula', $matricula);
+                    $stmt->bindValue(':nombre', $nombre);
+                    $stmt->bindValue(':apaterno', $apaterno);
+                    $stmt->bindValue(':amaterno', $amaterno);
+                    $stmt->bindValue(':tipousuario', $tipousuario);
+                    $stmt->bindValue(':sexo', $sexo);
+                    $stmt->bindValue(':edad', $edad);
+                    $stmt->bindValue(':telefono', $telefono);
+                    $stmt->bindValue(':email', $email);
+                    $stmt->bindValue(':contrasena', $contrasena);
+                
+                    $stmt->execute();
+                    echo "El registro del alumno $nombre $apaterno $amaterno se realizó con éxito.";
+                    echo "<br><br><a href='index.html'>*** Regresar al inicio ***</a>";
+                    $conn = null;
+                }catch(PDOException $e){
+                    echo $sql . "<br>" . $e->getMessage();
+                    echo "*** Ocurrió un error; valida los datos que estás ingresando ***";
+                }
+            }else{
+                echo "<br><br><a href='registro.html'>*** Verifica los datos que intenstaste ingresar y vuelve a intentar tu registro ***</a>";
             }
        }else{
-            echo "<br><br><a href='registro.html'>*** Verifica los datos que intenstaste ingresar y vuelve a intentar tu registro ***</a>";
-       }
-       
+            echo "Contraseña no válida: " . $error_encontrado .". La contraseña debe tener al menos 8 caracteres, letras, números y por lo menos un caracter especial (#,$,-,_,&,%).";
+            echo "<br><br><a href='registro.html'>*** Vuelve a intentarlo con una contraseña válida ***</a>";
+
+       }      
    }else{
         echo "El campo de contraseña y la confirmación de la contraseña no coinciden";
         echo "<br><br><a href='registro.html'>*** Vuelve a intentarlo ***</a>";
