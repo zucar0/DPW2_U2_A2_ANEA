d11e7a87a57a67de44a69faaeacc13ab9006e157
Antonio <antonio.enralv@gmail.com>
2021-02-28 19:07:55 -0600

    Se agrega archivo para validar formulario en php

--- guardar.php
+++ guardar.php
@@ -12,25 +12,30 @@
    $confirma=$_POST['confirma'];   
    $tipousuario='E';
 
-   try{
-    $stmt = $conn->prepare("INSERT INTO usuarios VALUES(:matricula, :nombre, :apaterno, :amaterno, :tipousuario, :sexo, :edad, :telefono, :email, :contrasena)");
-    $stmt->bindValue(':matricula', $matricula);
-    $stmt->bindValue(':nombre', $nombre);
-    $stmt->bindValue(':apaterno', $apaterno);
-    $stmt->bindValue(':amaterno', $amaterno);
-    $stmt->bindValue(':tipousuario', $tipousuario);
-    $stmt->bindValue(':sexo', $sexo);
-    $stmt->bindValue(':edad', $edad);
-    $stmt->bindValue(':telefono', $telefono);
-    $stmt->bindValue(':email', $email);
-    $stmt->bindValue(':contrasena', $contrasena);
-
-    $stmt->execute();
-    echo "El registro del alumno $nombre $apaterno $amaterno se realizó con éxito.";
-    echo "<br><br><a href='index.html'>*** Regresar al inicio ***</a>";
-    $conn = null;
-    }catch(PDOException $e){
-        echo $sql . "<br>" . $e->getMessage();
-    }
+   if($_POST["contrasena"] === $_POST["confirma"]){
+        try{
+            $stmt = $conn->prepare("INSERT INTO usuarios VALUES(:matricula, :nombre, :apaterno, :amaterno, :tipousuario, :sexo, :edad, :telefono, :email, :contrasena)");
+            $stmt->bindValue(':matricula', $matricula);
+            $stmt->bindValue(':nombre', $nombre);
+            $stmt->bindValue(':apaterno', $apaterno);
+            $stmt->bindValue(':amaterno', $amaterno);
+            $stmt->bindValue(':tipousuario', $tipousuario);
+            $stmt->bindValue(':sexo', $sexo);
+            $stmt->bindValue(':edad', $edad);
+            $stmt->bindValue(':telefono', $telefono);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':contrasena', $contrasena);
+        
+            $stmt->execute();
+            echo "El registro del alumno $nombre $apaterno $amaterno se realizó con éxito.";
+            echo "<br><br><a href='index.html'>*** Regresar al inicio ***</a>";
+            $conn = null;
+            }catch(PDOException $e){
+                echo $sql . "<br>" . $e->getMessage();
+            }
+   }else{
+        echo "El campo de contraseña y la confirmación de la contraseña no coinciden";
+        echo "<br><br><a href='registro.html'>*** Vuelve a intentarlo ***</a>";
+   }
     
 ?>
---
+++ validar.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST['submit'])){
+        if(empty($nombre)){
+            echo"<p class='error'> *** Agrega tu nombre *** </p>";
+        }
+    }
+
+?>
