721f87d23739133ca1bd6b03f2daad0ed3148565
Antonio <antonio.enralv@gmail.com>
2021-03-04 07:03:37 -0600

    se borra codigo que no servia en php

--- estudiante.php
+++ estudiante.php
@@ -7,20 +7,7 @@
     if($variableSesion ==null || $variableSesion =''){
         echo 'No tiene autorización para ingresar';
         die();
-    } 
-    $host       ="localhost";
-    $dbname     = "id16129776_progweb2";
-    $user       = "id16129776_antonioenriquez";
-    $password   ="zjo_%H=)I6S/s1fm";
-    //Conexión a la base de datos
-   $conexionE=mysqli_connect("localhost", $user, $password, $dbname );
-   $consultaE="SELECT * FROM usuarios WHERE matricula='$variableSesion'";
-   $resultadoE=mysqli_query($conexionE, $consultaE);
-   $filasE=mysqli_fetch_array($resultadoE);
-   $nombreE = $filasE['nombre'];
-   $apaternoE = $filasE['apaterno'];
-   $amaternoE = $filasE['amaterno'];
-   $tipousuarioE = $filasE['tipousuario'];
+    }
 
 ?>
 <!DOCTYPE html>
