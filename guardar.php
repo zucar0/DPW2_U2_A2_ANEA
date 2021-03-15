47d50bdf27cf5cc2b14842df9e9802acd8f8e840
Antonio <antonio.enralv@gmail.com>
2021-03-15 01:42:42 -0600

    captcha

--- guardar.php
+++ guardar.php
@@ -1,4 +1,37 @@
 <?php
+define("RECAPTCHA_V3_SECRET_KEY", 'YOUR_SECRET_HERE');
+if (isset($_POST['email']) && $_POST['email']) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+} else {
+    // set error message and redirect back to form...
+    header('location: registro.html');
+    exit;
+}
+
+$token = $_POST['token'];
+$action = $_POST['action'];
+
+// call curl to POST request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+ 
+// verify the response
+if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+    // valid submission
+    // go ahead and do necessary stuff
+} else {
+    // spam submission
+    // show error message
+}
+
+
+
    include "conn.php";
    $matricula=$_POST['matricula']; 
    $nombre=$_POST['nombre'];  
